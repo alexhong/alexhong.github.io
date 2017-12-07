a3c969ac7458cefaf997cb6e0dc7a3e9060d49f6
alexhong <alex@Alexs-MacBook-Pro.local>
2017-12-07 13:01:09 -0800

    added pchat

---
+++ pchat/script.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('[title]').each(function(){
+		var l = $(this).attr('title');
+		l >= 5000 ? $(this).addClass('a')
+		: l >= 1000 ? $(this).addClass('b')
+		: l >= 500 ? $(this).addClass('c') 
+		: l >= 100 ? $(this).addClass('d') 
+		: '';
+	});
+});
