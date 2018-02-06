5d264bc059ba36c5f9cdd3baaa0717856f896deb
alexhong <alexhong@users.noreply.github.com>
2018-02-06 00:52:45 -0800

    removed pchat

--- pchat/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-	Array.from(document.querySelectorAll('[title]')).map(function(a){
-		var l = a.getAttribute('title');
-		a.className = l>=5000 ? 'a' : l>=1000 ? 'b' : l>=500 ? 'c' : l>=100 ? 'd' : '';
-	});
-}
