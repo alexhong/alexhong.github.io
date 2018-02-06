c0fd9092cdf302385da8835b1e45a227deb4cf70
alexhong <alexhong@users.noreply.github.com>
2018-02-05 20:53:00 -0800

    script tweak

--- pchat/script.js
+++ pchat/script.js
@@ -1,6 +1,4 @@
 window.onload = function(){
-	if (location.protocol == 'http:') 
-		document.querySelectorAll('a.index').setAttribute('href','../');
 	Array.from(document.querySelectorAll('[title]')).map(function(a){
 		var l = a.getAttribute('title');
 		a.className = l>=5000 ? 'a' : l>=1000 ? 'b' : l>=500 ? 'c' : l>=100 ? 'd' : '';
