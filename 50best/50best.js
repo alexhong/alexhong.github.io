0bddf228f4207a32e5861824b258b4039f533ff6
alexhong <alexhong@users.noreply.github.com>
2022-11-18 15:46:46 -0500

    added country filter reset

--- 50best/50best.js
+++ 50best/50best.js
@@ -1,3 +1,6 @@
+document.getElementById('title').addEventListener("click",function(){
+	document.querySelectorAll('.fade').forEach(function(li){ li.classList.remove('fade'); });
+});
 document.querySelectorAll('.country').forEach(function(c){
 	var cc = c.classList[1];
 	c.parentNode.classList.add(cc);
