21a54d823e8d99f946bac092400b8d6b857394c8
alexhong <alexhong@users.noreply.github.com>
2022-11-18 15:37:41 -0500

    redo 50best js

---
+++ 50best/50best.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.country').forEach(function(c){
+	var cc = c.classList[1];
+	c.parentNode.classList.add(cc);
+	c.addEventListener("click",function(){
+		document.querySelectorAll('li').forEach(function(li){ li.classList.add('fade'); });
+		document.querySelectorAll('li.'+cc).forEach(function(li){ li.classList.remove('fade'); });
+	});
+});
